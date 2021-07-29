a02854990d83c3521327f0a4b4b03540d8253d4b
YunaH <YunaH503@gmail.com>
2021-07-28 22:20:43 -0400

    Added problem 1

---
+++ coding-problems/src/Problem1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/*
+Problem 1
+Given a list of numbers, return whether any two sums to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+*/
+
+public class Problem1 {
+
+    public static void main(String[] args) {
+
+    }
+}
